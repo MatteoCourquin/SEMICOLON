688e979201b3af6c0425e1a5a087102b02e492e4
“Mat-Techno” <matteo.courquin@gmail.com>
2021-11-12 23:32:46 +0100

    :lipstick: add CSS sections & :construction: done sections

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,6 +1,35 @@
 // **** SCRIPT **** //
 // ================ //
 
+// **** Line Scroll **** //
+
+window.onload = () => {
+    window.addEventListener("scroll", () => {
+        let hauteur = document.documentElement.scrollHeight - window.innerHeight;
+        let position = window.scrollY;
+        let largeur = document.documentElement.clientWidth;
+
+        let barre = (position / hauteur) * largeur;
+
+        document.getElementById("top-barre").style.width = barre + "px";
+    });
+};
+
+
+
+// **** Burger **** //
+
+const btn = document.querySelector('.burger-menu');
+const menu = document.querySelector('.nav-burger');
+
+btn.addEventListener('click', burger);
+
+function burger(){
+    menu.classList.toggle('active');
+}
+
+
+// **** GreenSocks Anims **** //
 
 const l1 = document.querySelector('.l1');
 const l2 = document.querySelector('.l2');
