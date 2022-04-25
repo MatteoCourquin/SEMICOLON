cf2929fced4377a61c8d98646c9e22755f2264c5
« Matteo <matteo.courquin@gmail.com>
2022-04-25 17:39:49 +0200

    Animation to sections

--- assets/script/script.js
+++ assets/script/script.js
@@ -16,3 +16,33 @@ anime.timeline({ loop: false }).add({
         return 900 + 50 * i;
     },
 });
+
+
+
+
+
+
+
+let sections = document.querySelectorAll('.section-to-anim')
+
+sections.forEach(section => {
+
+    let title = document.querySelector(`#${section.id} .title`)
+    let text1 = document.querySelector(`#${section.id} .text1`)
+    let text2 = document.querySelector(`#${section.id} .text2`)
+
+    const TL = gsap.timeline({
+        scrollTrigger: {
+            markers: true,
+            trigger: title,
+            start: "top 60%",
+            end: "bottom top"
+        }
+    })
+    
+    TL
+    .to(title, {duration: 1, opacity: 1, y: 0})
+    .to(text1, {duration: 1, opacity: 1, y: 0}, '-=0.75')
+    .to(text2, {duration: 1, opacity: 1, y: 0}, '-=0.75')
+
+})
