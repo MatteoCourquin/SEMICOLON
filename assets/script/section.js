15d9d88a48a5bdb07f28f493db2c8656e9ea41b1
« Matteo <matteo.courquin@gmail.com>
2022-04-28 12:04:43 +0200

    :rocket:

--- assets/script/section.js
+++ assets/script/section.js
@@ -94,11 +94,14 @@ function getCurrenAnimation(fnstring) {
 // Scroll 
 
 let btnScroll = document.querySelector('.arrow-scroll-button')
-let rootElement = document.documentElement;
+let scrollButton = document.querySelector('.scroll-button')
 let firstSection = document.querySelector('.second')
 let lastSection = document.querySelector('.seventh')
+let rootElement = document.documentElement;
 
+scrollButton.addEventListener('click', () => {
 
+})
 gsap.to(btnScroll, {
     right: 20,
     scrollTrigger: {
@@ -125,14 +128,14 @@ btnScroll.addEventListener("click", (e) => {
     });
 });
 
-
-btnScroll.addEventListener('click', () => {
+function scrollClientHeight() {
     let scrollDistance = document.documentElement.clientHeight;
     window.scrollBy({
         top: scrollDistance,
         behavior: 'smooth'
     })
-})
-
+}
+scrollButton.addEventListener("click", scrollClientHeight)
+btnScroll.addEventListener("click", scrollClientHeight)
 
 gsap.to(btnScroll, {y: '-10', repeat: -1, yoyo: true, duration: 0.6, ease: 'power1.inOut'})
